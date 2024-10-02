7df48d346adeb7f2622c903a375b9953a9a2ea1f
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-10-02 18:56:32 +0200

    Removed deprecated error function

--- composeApp/src/desktopMain/kotlin/org/openswim/serialcom/SerialComm.kt
+++ composeApp/src/desktopMain/kotlin/org/openswim/serialcom/SerialComm.kt
@@ -3,19 +3,27 @@ package org.openswim
 import com.fazecast.jSerialComm.SerialPort
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-private val LoggerSingleton = KotlinLogging.logger{}
 
-class SerialComm(val port: SerialPort, val iBaudRate: Int = 19200, val iNDataBits: Int = 8, val iNStopBits: Int = 1, val iParity: Int = SerialPort.NO_PARITY) {
+private val LoggerSingleton = KotlinLogging.logger {}
+
+class SerialComm(
+    val port: SerialPort,
+    val iBaudRate: Int = 19200,
+    val iNDataBits: Int = 8,
+    val iNStopBits: Int = 1,
+    val iParity: Int = SerialPort.NO_PARITY
+) {
     init {
         port.setComPortParameters(iBaudRate, iNDataBits, iNStopBits, iParity);
         port.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
         port.openPort()
     }
+
     fun sendString(strData: String, strEndCommand: String = "") {
         val fullCmdBuilder = StringBuilder()
         fullCmdBuilder.append(strData).append(strEndCommand)
         var strFullCommand = fullCmdBuilder.toString()
-        LoggerSingleton.debug{"Sending message ${strFullCommand}"}
+        LoggerSingleton.debug { "Sending message ${strFullCommand}" }
         val bCommand = strFullCommand.toByteArray()
         try {
             port.writeBytes(bCommand, bCommand.size)
@@ -24,7 +32,8 @@ class SerialComm(val port: SerialPort, val iBaudRate: Int = 19200, val iNDataBit
             throw e
         }
     }
-    fun receiveString(strEndCommand: String? = null) : String {
+
+    fun receiveString(strEndCommand: String? = null): String {
         val buffer = ByteArray(1024)
         val responseBuilder = StringBuilder()
         var numRead: Int
@@ -37,14 +46,15 @@ class SerialComm(val port: SerialPort, val iBaudRate: Int = 19200, val iNDataBit
                 }
             } while (strEndCommand != null && !responseBuilder.endsWith(strEndCommand))
         } catch (e: Exception) {
-            LoggerSingleton.error("Failed to receive data: ${e.message}")
+            LoggerSingleton.error { "Failed to receive data: ${e.message}" }
             throw e
         }
 
         val strResponse = responseBuilder.toString()
-        LoggerSingleton.debug{"Received message: ${strResponse}"}
+        LoggerSingleton.debug { "Received message: ${strResponse}" }
         return strResponse
     }
+
     fun close() {
         port.closePort()
     }
