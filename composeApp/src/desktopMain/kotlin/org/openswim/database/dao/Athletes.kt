2d8d2a9c77f3e826ce9360d7c817f684ae14caf4
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-10-05 15:16:33 +0200

    Added clubs reference to athletes

--- composeApp/src/desktopMain/kotlin/org/openswim/database/dao/Athletes.kt
+++ composeApp/src/desktopMain/kotlin/org/openswim/database/dao/Athletes.kt
@@ -11,8 +11,15 @@ object Athletes : IntIdTable("athletes") {
     val name = varchar("name", 255)
     val surname = varchar("surname", 255)
     val sex = varchar("sex", 1)
+    val cf = varchar("cf", 16).nullable()
+    val fin_code = varchar("fin_code", 50).nullable()
+    val uisp_code = varchar("uisp_code", 50).nullable()
+    val country = varchar("country", 50).nullable()
+    val email = varchar("email", 255).nullable()
     val birthDate = date("birthdate")
-    val categoryId = reference("category_id", Categories, onDelete = ReferenceOption.CASCADE)
+    val clubId = reference("club_id", Clubs, onDelete = ReferenceOption.SET_NULL, onUpdate = ReferenceOption.CASCADE)
+    val categoryId =
+        reference("category_id", Categories, onDelete = ReferenceOption.SET_NULL, onUpdate = ReferenceOption.CASCADE)
 }
 
 class Athlete(id: EntityID<Int>) : IntEntity(id) {
@@ -21,7 +28,13 @@ class Athlete(id: EntityID<Int>) : IntEntity(id) {
     var name by Athletes.name
     var surname by Athletes.surname
     var sex by Athletes.sex
+    var cf by Athletes.cf
+    var finCode by Athletes.fin_code
+    var uispCode by Athletes.uisp_code
+    var country by Athletes.country
+    var email by Athletes.email
     var birthDate by Athletes.birthDate
+    var club by Club referencedOn Athletes.clubId
     var category by Category referencedOn Athletes.categoryId
 }
 
