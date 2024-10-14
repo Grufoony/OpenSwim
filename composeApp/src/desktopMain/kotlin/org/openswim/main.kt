9d83648775cca8b008e8927d44d6b90de0b3ca91
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-10-14 15:28:57 +0200

    Removed db test

--- composeApp/src/desktopMain/kotlin/org/openswim/main.kt
+++ composeApp/src/desktopMain/kotlin/org/openswim/main.kt
@@ -3,35 +3,11 @@ package org.openswim
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-import Athletes
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.transactions.transaction
-
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "OpenSwim",
     ) {
-        Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
-
-        transaction {
-            SchemaUtils.create(Athletes)
-
-            val athleteId = Athletes.insert {
-                it[name] = "Mario"
-                it[surname] = "Rossi"
-                it[sex] = "M"
-            } get Athletes.id
-
-            val secondAthleteId = Athletes.insert {
-                it[name] = "Maria"
-                it[surname] = "Bianchi"
-                it[sex] = "F"
-            } get Athletes.id
-
-            println("Created new athletes with ids $athleteId and $secondAthleteId.")
-        }
         App()
     }
 }
